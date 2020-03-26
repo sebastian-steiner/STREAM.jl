d87bac53fd5bfea8c9b143915bd69e310ed5970a
Thesis Student <steiner@hydra.par.tuwien.ac.at>
2020-03-26 12:11:09 +0100

    Added dir for past results

--- stream.jl
+++ stream.jl
@@ -143,8 +143,8 @@ function main()
     checkResults(a, b, c)
 
 	open("jl.results", "w") do f
-        # print all times to disk
-        Printf.@printf f "Copy;Scale;Add;Triad\n"
+        	# print all times to disk
+	        Printf.@printf f "Copy;Scale;Add;Triad\n"
 		for k in 1:NTIMES
 			Printf.@printf f "%11.6f;%11.6f;%11.6f;%11.6f\n" times[1,k] times[2,k] times[3,k] times[4,k]
 		end
